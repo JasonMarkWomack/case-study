ab8346d7f1ea99f00dd767e355249c00f3186359
Jason Womack <jasonwomack@Jasons-MacBook-Pro.local>
2024-02-09 18:22:58 -0500

    need figure out pop up sound modal

--- my-app/src/pages/_document.js
+++ my-app/src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+const axios = require('axios');
+const id = "4f6a6ece-691e-42f2-bb20-c46df2413115"; 
+const API_KEY = '2a911e1c-8e20-40c9-b953-e8b23aa5b030';
+export default function Document(){
+ function handClickFunction  () {
+  return axios.get(`https://api.vapi.ai/call/${id}`, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`
+    }
+  })
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+return(  <button onClick={handClickFunction()}>Click Here for for Sciencetist Calls</button>
+)
 }
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -5,18 +5,22 @@ const inter = Inter({ subsets: ["latin"] });
 const axios = require('axios');
 const id = "4f6a6ece-691e-42f2-bb20-c46df2413115"; 
 const API_KEY = '2a911e1c-8e20-40c9-b953-e8b23aa5b030';
-  axios.get(`https://api.vapi.ai/call/${id}`, {
+export default function Home(){
+ function handClickFunction  () {
+  return axios.get(`https://api.vapi.ai/call/${id}`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`
     }
   })
-  .then(response => {
-    console.log(response.data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
 
 
-  <button onClick={handleClick}>Click for Science</button>
+  <button onClick={handClickFunction()}>Click for Science</button>
 
+}
