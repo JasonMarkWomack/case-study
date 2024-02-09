bce5689fc8c8c0d1247ec89adbd6e57e5e846177
Jason Womack <jasonwomack@Jasons-MacBook-Pro.local>
2024-02-09 18:06:43 -0500

    pompt is console logging / need to do sound

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,29 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 const axios = require('axios');
+const id = "4f6a6ece-691e-42f2-bb20-c46df2413115"; 
 const API_KEY = '2a911e1c-8e20-40c9-b953-e8b23aa5b030';
+  axios.get(`https://api.vapi.ai/call/${id}`, {
+    headers: {
+      Authorization: `Bearer ${API_KEY}`
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-const res = await axios.get('https://api.vapi.ai/assistant', {
-  headers: {
-    Authorization: `Bearer ${API_KEY}`
-  }
-});
 
+  <button onClick={handleClick}>Click for Science</button>
 
-
-
-const handleClick = (e) =>  {
-  console.log('rando');
-  console.log(e);
-}
-
-const makeCall = () => (
-
-  <div>
-      <button onClick={handleClick}>Add</button>
-  </div>
-
-)
-export default makeCall;
